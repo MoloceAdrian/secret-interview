73b4eebab1e079599dd62d6767c6d26c8f989189
Adrian Moloce <moloce.ady@gmail.com>
2020-12-18 19:47:31 +0200

    Applied some formatting using black.

--- binary_reverse.py
+++ binary_reverse.py
@@ -5,55 +5,59 @@ import time
 
 BUFSIZE = 1 << 15
 
+
 def output_file_name(file_name: str):
     """Remove file extension and replace it with '.out'.
-    
+
     Args:
         file_name: str -> name of the file containing extension.
     Returns:
         str: given file name with its extension changed to '.out'
     """
-    return file_name.split('.')[0] + '.out'
+    return file_name.split(".")[0] + ".out"
+
 
 # Implementation for O(n) time, O(1) space.
 def create_reversed_bytes_position_file(file_name: str):
     """Create a new file which contains the bytes from a given file in reverse location.
-    
+
     Args:
         file_name: str -> name of the file to read data from.
     """
     try:
-        with open(file_name, 'rb') as file_in, open(output_file_name(file_name), 'wb') as file_out:
+        with open(file_name, "rb") as file_in, open(
+            output_file_name(file_name), "wb"
+        ) as file_out:
             # Go to the end of the file.
-            print(f'The size of the file is [{sys.getsizeof(file_in)}]. \n')
-            print(f'The output is being stored in [{output_file_name(file_name)}]. \n')
+            print(f"The size of the file is [{sys.getsizeof(file_in)}]. \n")
+            print(f"The output is being stored in [{output_file_name(file_name)}]. \n")
 
-            file_in.seek(0, os.SEEK_END)   
+            file_in.seek(0, os.SEEK_END)
             for position in reversed(range(0, file_in.tell(), BUFSIZE)):
                 file_in.seek(position, os.SEEK_SET)
                 file_out.write(file_in.read(BUFSIZE)[::-1])
 
-            print('Output file created successfully!\n')
+            print("Output file created successfully!\n")
     except FileNotFoundError as e:
-        print('Something went wrong, check the traceback for more details ... \n')
-        print(e, '\n')
+        print("Something went wrong, check the traceback for more details ... \n")
+        print(e, "\n")
 
 
 def main():
     # Ignore the first argument as it's the name of the module.
     for file_name in sys.argv[1:]:
         start = time.time()
-        print('-' * 70)
-        print(f'Started reversal process for file [{file_name}]. \n')
-        
+        print("-" * 70)
+        print(f"Started reversal process for file [{file_name}]. \n")
+
         create_reversed_bytes_position_file(file_name)
-        
+
         end = time.time()
-        print(f'The process took {end - start}.')
-        print('-' * 70)
+        print(f"The process took {end - start}.")
+        print("-" * 70)
 
 
 if __name__ == "__main__":
-    print('\nStarted file collection and creation...\n')
+    print("\nStarted file collection and creation...\n")
     main()
-    print('End of program...\n')
+    print("End of program...\n")
--- tests/unit/test_create_reversed_byte_position_file.py
+++ tests/unit/test_create_reversed_byte_position_file.py
@@ -3,28 +3,35 @@ import pytest
 from binary_reverse import create_reversed_bytes_position_file
 
 
-@pytest.mark.parametrize("test_input, test_output, expected", [
-    ('data/test_1b.in', 'data/test_1b.out', 'data/expected_1b'),
-    ('data/test_1kb.in', 'data/test_1kb.out', 'data/expected_1kb'),
-    ('data/test_50kb.in', 'data/test_50kb.out', 'data/expected_50kb'),
-    ('data/test_500kb.in', 'data/test_500kb.out', 'data/expected_500kb'),
-    ('data/test_10mb.in', 'data/test_10mb.out', 'data/expected_10mb'),
-])
-def test_can_create_new_file_with_reversed_bytes_position(test_input, test_output, expected):
+@pytest.mark.parametrize(
+    "test_input, test_output, expected",
+    [
+        ("data/test_1b.in", "data/test_1b.out", "data/expected_1b"),
+        ("data/test_1kb.in", "data/test_1kb.out", "data/expected_1kb"),
+        ("data/test_50kb.in", "data/test_50kb.out", "data/expected_50kb"),
+        ("data/test_500kb.in", "data/test_500kb.out", "data/expected_500kb"),
+        ("data/test_10mb.in", "data/test_10mb.out", "data/expected_10mb"),
+    ],
+)
+def test_can_create_new_file_with_reversed_bytes_position(
+    test_input, test_output, expected
+):
 
     create_reversed_bytes_position_file(test_input)
-    
+
     assert filecmp.cmp(test_output, expected) == True
+
 
 def test_reverse_byte_location_works_for_empty_file():
 
-    create_reversed_bytes_position_file('data/test_empty.in')
-    
-    assert filecmp.cmp('data/test_empty.out', 'data/expected_empty' ) == True
+    create_reversed_bytes_position_file("data/test_empty.in")
+
+    assert filecmp.cmp("data/test_empty.out", "data/expected_empty") == True
+
 
 def test_reverse_byte_location_fails_gracefully_for_non_existing_file():
     try:
-        create_reversed_bytes_position_file('non-existing-file.in')
+        create_reversed_bytes_position_file("non-existing-file.in")
         # Passed this line successfully.
         assert True
     except Exception:
